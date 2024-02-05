ffbce032104fac25a18137250e732b303785524f
Sachin <sachin@codewalnut.com>
2024-02-05 15:42:19 +0530

    adding date range, heading, placeholder, week start option

--- src/components/molecules/MyComponent/MyComponent.tsx
+++ src/components/molecules/MyComponent/MyComponent.tsx
@@ -1,9 +1,16 @@
 import { CustomDatePicker } from "@/components/atoms/DatePicker/DatePicker";
 
 export function MyComponent() {
+  const endDateLimit = new Date();
+  endDateLimit.setDate(endDateLimit.getDate() + 30);
   return (
     <div className="w-full flex justify-center">
-      <CustomDatePicker isEndDate />
+      <CustomDatePicker isEndDate 
+                        startDatePlaceHolder="Start Date"
+                        endDatePlaceHolder="End Date"
+                        calendarStartFromMondayDay={false}
+                        endDateLimit={endDateLimit}
+                          />
     </div>
   );
 }
