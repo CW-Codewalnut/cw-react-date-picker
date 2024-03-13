607d2f24e2a2fae238595f1e23ebc62c52d4f2a2
Sarath G <sarathkumarg.in@gmail.com>
2024-03-13 18:48:39 +0530

    refactor(layout): removed unwanted element from the dom

--- src/components/organisms/MainContent/MainContent.tsx
+++ src/components/organisms/MainContent/MainContent.tsx
@@ -5,5 +5,5 @@ interface MainContentProps {
 }
 
 export function MainContent({ children }: MainContentProps) {
-  return <main className="main-content w-full relative top-40 h-auto mx-auto">{children}</main>;
+  return <main className="main-content w-full relative inset-y-28 mb-40 mx-auto flex flex-col px-3 md:px-4">{children}</main>;
 }
