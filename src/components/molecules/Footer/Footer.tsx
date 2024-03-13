f84a691d9122d036b6d321a2ae5df8a446e7b2e6
Sarath G <sarathkumarg.in@gmail.com>
2024-03-13 18:48:39 +0530

    refactor(layout): removed unwanted element from the dom

--- src/components/molecules/Footer/Footer.tsx
+++ src/components/molecules/Footer/Footer.tsx
@@ -1,13 +1,9 @@
 export function Footer() {
     return (
       <footer className="fixed bottom-0 z-50 w-full bg-black p-4 shadow-md text-gray-400 dark:bg-gray-900 dark:focus:ring-gray-600">
-        <div className="flex justify-between">
-          <div>
-            <div>&copy; 2023 CodeWalnut. All rights reserved.</div>
-          </div>
-          <div className="flex flex-col items-end">
-            <div>CodeWalnut is a brand of Bizboost Business Solutions LLP.</div>
-          </div>
+        <div className="flex justify-between flex-col text-center gap-2 md:text-start md:flex-row">
+            <p className="text-xs md:text-sm">&copy; 2023 CodeWalnut. All rights reserved.</p>
+            <p className="text-xs md:text-sm">CodeWalnut is a brand of Bizboost Business Solutions LLP.</p>
         </div>
       </footer>
     );
